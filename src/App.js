19909e57fe746852248c0c162e3ef8e324f9d49a
nouherg <nouhereguigui@gmail.com>
2022-07-07 18:33:56 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Form from "./component/Form";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
